5206c587d1ff12ba73b17b163f89c8251d736af4
PESOKOTiK <alexey.pylypenko007@gmail.com>
2023-09-09 10:27:15 +0200

    Lab work 1.3

--- laba1.1.py
+++ laba1.1.py
@@ -10,7 +10,7 @@ else:
     x = (a * 5 - 1) / a
 
 print("X = " + str(x))
-
+print()
 # 2 task
 x = 0.0
 print("x   |  y")
@@ -18,3 +18,22 @@ while x <= 4.0:
     y = x
     x += 0.5
     print(str(x) + " | "+str(y))
+print()
+# 3 task
+n = 5
+for i in range(1, n + 1):
+    print(8*" ", end='')
+    for j in range(i, 0, -1):
+        print(j, end=' ')
+    for k in range(n - i):
+        print(' ', end=' ')
+    print()
+for i in range(1, n + 1):
+    for j in range(1, i):
+        print(" ", end=" ")
+    for k in range(1, n - i + 2):
+        if k < n - i + 1:
+            print(k, end=" ")
+        else:
+            print(k, end="")
+    print()

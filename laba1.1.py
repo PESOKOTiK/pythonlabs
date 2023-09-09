8ab6942276ab8f10357f522c2ef96a200de2f5d4
PESOKOTiK <alexey.pylypenko007@gmail.com>
2023-09-09 10:05:03 +0200

    Lab work 1.2

--- laba1.1.py
+++ laba1.1.py
@@ -1,3 +1,4 @@
+# 1 Task
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 x = 0
@@ -9,3 +10,11 @@ else:
     x = (a * 5 - 1) / a
 
 print("X = " + str(x))
+
+# 2 task
+x = 0.0
+print("x   |  y")
+while x <= 4.0:
+    y = x
+    x += 0.5
+    print(str(x) + " | "+str(y))
